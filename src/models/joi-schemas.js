3e7f491578ee23ea46097403f582508be181488b
Marcos Oliveira Gomes <115948731+marcosogg@users.noreply.github.com>
2024-05-16 20:04:58 +0100

    sanitisation and hashing

--- src/models/joi-schemas.js
+++ src/models/joi-schemas.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const IdSpec = Joi.alternatives().try(Joi.string(), Joi.object()).description("a valid ID");
 
 export const UserCredentialsSpec = {
-  email: Joi.string().email().required(),
+  email: Joi.string().email().lowercase().required().trim(),
   password: Joi.string().required()
 };
 
@@ -11,7 +11,7 @@ export const UserSpec = Joi.object()
   .keys({
     firstName: Joi.string().example("Homer").required(),
     lastName: Joi.string().example("Simpson").required(),
-    email: Joi.string().email().example("homer@simpson.com").required(),
+    email: Joi.string().email().lowercase().required().trim().example("homer@simpson.com"),
     password: Joi.string().example("secret").required(),
     _id: IdSpec,
     __v: Joi.number()
@@ -21,11 +21,11 @@ export const UserSpec = Joi.object()
 export const UserArray = Joi.array().items(UserSpec).label("UserArray");
 
 export const PlaceSpec = {
-  place: Joi.string().required(),
-  category: Joi.string().required(),
-  description: Joi.string().required(),
-  lat: Joi.number().allow(".").required(),
-  long: Joi.number().allow(".").required(),
+  place: Joi.string().required().trim(),
+  category: Joi.string().required().trim(),
+  description: Joi.string().required().trim(),
+  lat: Joi.number().min(-90).max(90).required(),  // Valid latitude range
+  long: Joi.number().min(-180).max(180).required()  // Valid longitude range
 };
 
 export const CollectionSpec = {
